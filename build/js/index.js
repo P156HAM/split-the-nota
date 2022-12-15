567badbeb32be125226937205031bc53583e7dbf
P156HAM <a.hamdi.almasalmeh@gmail.com>
2022-12-15 11:04:22 +0100

    Last update

--- build/js/index.js
+++ build/js/index.js
@@ -1,22 +1,26 @@
 function calculateTip(sum, tip) {
+    console.log(sum);
+    console.log(tip);
     const calculatedTip = sum * tip;
+    console.log(calculatedTip);
     const total = sum + calculatedTip;
     return total;
 }
 function divideTotal(total, numberOfFriends) {
-    const sumDivided = total / numberOfFriends;
+    const sumDivided = Number(total / numberOfFriends);
     return sumDivided;
 }
-function showDividedSum(friendSum) {
-    const sum = parseInt(document.getElementById('sum').value);
-    const tip = parseInt(document.getElementById('tip').value);
+let friendSum;
+document.getElementById('calculateButton').addEventListener('click', function () {
+    const sum = Number(document.getElementById('sum').value);
+    const tip = Number(document.getElementById('tip').value);
+    const numberOfFriends = Number(document.getElementById('numberOfFriends').value);
     const total = calculateTip(sum, tip);
-    const numberOfFriends = parseInt(document.getElementById('numberOfFriends').value);
     friendSum = divideTotal(total, numberOfFriends);
-    document.getElementById('calculateButton').addEventListener('click', function () {
-        document.getElementById('showSum').classList.toggle('hide');
-        document.getElementById('inputForm').classList.toggle('hide');
-        document.getElementById('friendSum').innerHTML = friendSum + ' kr';
-    });
+    document.getElementById('showSum').classList.toggle('hide');
+    document.getElementById('inputForm').classList.toggle('hide');
+    showDividedSum(friendSum);
+});
+function showDividedSum(sum) {
+    document.getElementById('friendSum').innerHTML = sum + ' kr';
 }
-showDividedSum(friendSum);
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
 
 
 function calculateTip(sum: number, tip: number): number {
+    console.log(sum);
+    console.log(tip);
     const calculatedTip: number = sum * tip; 
+    console.log(calculatedTip);
     const total: number =  sum + calculatedTip;
     return total;
 }
 
-
-
 function divideTotal(total: number, numberOfFriends: number) {
-    const sumDivided: number = total / numberOfFriends;
+    const sumDivided: number = Number(total / numberOfFriends);
     return sumDivided;
 }
+let friendSum: number;
 
-
-
-function showDividedSum(friendSum: number): void {
-    const sum = parseInt((document.getElementById('sum') as HTMLInputElement).value);
-    const tip = parseInt((document.getElementById('tip') as HTMLInputElement).value);
+document.getElementById('calculateButton').addEventListener('click', function() {
+    const sum = Number((document.getElementById('sum') as HTMLInputElement).value);
+    const tip = Number((document.getElementById('tip') as HTMLInputElement).value);
+    const numberOfFriends: number = Number((document.getElementById('numberOfFriends') as HTMLInputElement).value)
     const total: number = calculateTip(sum, tip)
-    const numberOfFriends: number = parseInt((document.getElementById('numberOfFriends') as HTMLInputElement).value)
     friendSum = divideTotal(total, numberOfFriends)
-    document.getElementById('calculateButton').addEventListener('click', function() {
-        document.getElementById('showSum').classList.toggle('hide');
-        document.getElementById('inputForm').classList.toggle('hide');
-        document.getElementById('friendSum').innerHTML = friendSum + ' kr';
-    });    
+    document.getElementById('showSum').classList.toggle('hide');
+    document.getElementById('inputForm').classList.toggle('hide');
+    showDividedSum(friendSum)
+});
+
+function showDividedSum(sum: number): void {
+    document.getElementById('friendSum').innerHTML = sum + ' kr';
 }
 
-showDividedSum(friendSum);
+
+
